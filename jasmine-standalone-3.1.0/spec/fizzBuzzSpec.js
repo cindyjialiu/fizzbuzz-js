e9ea8447f540f1b8b9998f9d7a8b6f556ee5822f
Cindy <jl7e12@gmail.com>
2018-03-20 22:36:54 +0000

    all tests passed

--- jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
@@ -10,12 +10,21 @@ describe('FizzBuzz', function() {
     it('fizzes for 3', function(){
       expect(fizzbuzz.play(3)).toEqual('Fizz');
     });
+  });
   describe('multiple of 5', function(){
     it('buzzes for 5', function(){
       expect(fizzbuzz.play(5)).toEqual('Buzz')
     });
   });
-
+  describe('multiple of 15', function(){
+    it('fizzbuzzes for 15', function(){
+      expect(fizzbuzz.play(15)).toEqual('FizzBuzz')
+    });
+  });
+  describe('all other numbers', function(){
+    it('1 for 1', function(){
+      expect(fizzbuzz.play(1)).toEqual(1)
+    });
   });
 
 });
