5cddd3e69f3c4a72aabf2f837381b95f3561988e
Cindy <jl7e12@gmail.com>
2018-03-20 15:41:42 +0000

    fizz and buzz tests passed

--- jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
@@ -1,3 +1,21 @@
-describe('', function() {
-  
+describe('FizzBuzz', function() {
+
+  var fizzBuzz;
+
+  beforeEach(function(){
+    fizzbuzz = new FizzBuzz
+  });
+
+  describe('multiple of 3', function(){
+    it('fizzes for 3', function(){
+      expect(fizzbuzz.play(3)).toEqual('Fizz');
+    });
+  describe('multiple of 5', function(){
+    it('buzzes for 5', function(){
+      expect(fizzbuzz.play(5)).toEqual('Buzz')
+    });
+  });
+
+  });
+
 });
